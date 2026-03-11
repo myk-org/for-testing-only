74b549950fdc078ea6ff72b7f81bf1ed00ffbd63
Meni Yakove <myakove@gmail.com>
2026-03-11 18:14:29 +0200

    Revert "test: add e2e_incremental_test_function for incremental docs test"
    This reverts commit c55b4c0425af489c354edf09c1718e21c4420b32.

--- src/request_handler/handler.py
+++
@@ -1,9 +0,0 @@
-
-
-def e2e_incremental_test_function():
-    """This function was added by the e2e incremental update test.
-
-    It verifies that incremental documentation updates detect new code
-    and include it in the generated documentation.
-    """
-    return "e2e-incremental-marker-12345"
