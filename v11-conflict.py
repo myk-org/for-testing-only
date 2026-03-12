db066e74f37c8dc485c10eaa0995def753e6a815
Meni Yakove <441263+myakove@users.noreply.github.com>
2026-03-12 17:34:09 +0200

    test: v11-conflict.py on main (#166)

--- v11-conflict.py
+++ v11-conflict.py
@@ -1,7 +1,8 @@
 class Queue:
-    def __init__(self):
+    def __init__(self, max_size=10):
         self.items = []
-        self.max_size = 10
+        self.max_size = max_size
+        self.total_processed = 0
 
     def push(self, item):
         if len(self.items) < self.max_size:
@@ -9,5 +10,9 @@ class Queue:
 
     def pop(self):
         if self.items:
+            self.total_processed += 1
             return self.items.pop(0)
         return None
+
+    def size(self):
+        return len(self.items)
