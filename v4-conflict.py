962b9c3f447d18e8aceb5acc0bc81e65f8eb772c
Meni Yakove <441263+myakove@users.noreply.github.com>
2026-03-12 15:59:43 +0200

    test: v4-conflict.py on main (#127)

--- v4-conflict.py
+++ v4-conflict.py
@@ -1,7 +1,8 @@
 class Cache:
-    def __init__(self):
+    def __init__(self, max_size=100):
         self.store = {}
         self.hits = 0
+        self.max_size = max_size
 
     def get(self, key):
         if key in self.store:
@@ -10,4 +11,11 @@ class Cache:
         return None
 
     def set(self, key, value):
+        if len(self.store) >= self.max_size:
+            oldest = next(iter(self.store))
+            del self.store[oldest]
         self.store[key] = value
+
+    def clear(self):
+        self.store.clear()
+        self.hits = 0
