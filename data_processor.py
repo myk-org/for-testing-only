005e07cb13662c0cced0fca949d6b7b1c478c681
Meni Yakove <myakove@gmail.com>
2026-06-22 19:00:01 +0300

    add simple DataProcessor

---
+++ data_processor.py
@@ -0,0 +1,18 @@
+class DataProcessor:
+    def __init__(self, config):
+        self.config = config
+        self.data = []
+
+    def process(self, items):
+        result = []
+        for item in items:
+            if item.is_valid():
+                result.append(self._transform(item))
+        return result
+
+    def _transform(self, item):
+        return {"id": item.id, "value": item.value * 2}
+
+    def export(self, format="json"):
+        import json
+        return json.dumps(self.data)
