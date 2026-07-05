c6925319b323bc9bd0ed9e3bd43eb77ecdf53ef4
Meni Yakove <441263+myakove@users.noreply.github.com>
2026-07-05 10:51:10 +0300

    test: add file for deletion test

---
+++ delete-me-test.py
@@ -0,0 +1,11 @@
+# Delete Me Test File
+
+def hello():
+    print('This file should be deleted by cherry-pick')
+
+def goodbye():
+    print('Farewell')
+
+if __name__ == '__main__':
+    hello()
+    goodbye()
